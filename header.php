d9af94f25ded26401ebb6d6d1dde4e76094dc655
Lars Boesenach <lars@lokaalgevonden.nl>
2016-04-07 20:26:50 +0200

    header placement done

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<meta charset="UTF-8">
-	<title>Lars Boesenach's Portfolio</title>
+	<title>Ammelier</title>
 	
 	
 	<!-- jQuery link -->
@@ -22,11 +22,12 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
 	<!-- Stylesheet -->
-	<link rel="stylesheet" href="style.css" />
+	<link rel="stylesheet" href="css/main.css" />
 
 
 	<!-- Fonts -->
 	<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,300,700' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Lato:400,300,100,300italic,400italic,100italic,700,700italic' rel='stylesheet' type='text/css'>
 
 
 
--- index.php
+++ index.php
@@ -9,12 +9,49 @@
 </head>
 <body>
 
-<div id="welcome">
-   your content on screen 1
+<div class="fullscreen-content-wrap one">
+	<div class="bg-img"></div>
+	<div class="darkscreen"></div>
+   
+   <div class="logo-wrap">
+   		<div class="logo">
+   			<div class="img"></div>
+   		</div>
+   </div>
+
+   <div class="header-content">
+   		<div class="header-text">
+   			<h1>Eat with confidence</h1>
+   			<div class="p-text">
+	   			<p>Ammelier is a service for people with allergies and don't like to book their restaurants, we have a solution for this, we call it Ammelier.</p>
+	   		</div>
+   		</div>
+   </div>
+
+   <div class="Ancordown">
+   		<a href="#ancor2">
+            <div class="arrow"></div>
+         </a>
+      		<p>Find out how it works</p>
+   </div>
 </div>
- 
-<div id="projects">
-   your content on screen 2
+
+<div class="content-wrap" id="ancor2">
+   <div class="container">
+      
+      <div class="row">
+         <div class="col-md-7">
+            
+         </div>
+         
+         <div class="col-md-5">.col-md-4</div>
+      </div>
+
+   </div>
+</div>
+
+<div class="fullscreen-content-wrap three">
+   your content on screen 4
 </div>
 
 </body>
