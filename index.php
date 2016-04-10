54f08d63ce9eed8e145576e80b77ef308c9b6b2d
Lars Boesenach <lars@lokaalgevonden.nl>
2016-04-10 02:00:14 +0200

    did some more stuff...

--- index.php
+++ index.php
@@ -15,7 +15,9 @@
    
    <div class="logo-wrap">
    		<div class="logo">
-   			<div class="img"></div>
+   			<div class="img">
+               <img src="img/logo_white.png">      
+            </div>
    		</div>
    </div>
 
@@ -32,23 +34,83 @@
    		<a href="#ancor2">
             <div class="arrow"></div>
          </a>
-      		<p>Find out how it works</p>
+      		<p>find out how it works</p>
    </div>
 </div>
+
 
 <div class="content-wrap" id="ancor2">
    <div class="container">
       
       <div class="row">
          <div class="col-md-7">
-            
+            <div class="text-box">
+               <h2>We understand nutrition</h2>
+                  <div class="text">
+                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Ammelier is a service for people with allergies and don't like to book their restaurants, we have a solution for this, we call it Ammelier.</p>
+                  </div>
+               <div class="button"><a href="#">visit blog</a></div>
+            </div>
          </div>
          
-         <div class="col-md-5">.col-md-4</div>
+         <div class="col-md-5">
+            <div class="text-box">
+               <div class="box-img"></div>
+            </div>
+         </div>
       </div>
 
    </div>
 </div>
+
+
+<div class="content-wrap secondwrap">
+   <div class="bg-img"></div>
+   <div class="darkscreen"></div>
+   
+   <div class="container">
+      <div class="row">
+         <div class="col-md-5"></div>
+
+         <div class="col-md-7">
+            <div class="text-box">
+               <h2>We understand nutrition</h2>
+                  <div class="text">
+                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Ammelier is a service for people with allergies and don't like to book their restaurants, we have a solution for this, we call it Ammelier.</p>
+                  </div>
+               <div class="button"><a href="#">check website</a></div>
+            </div>
+         </div>         
+      </div>
+   </div>
+</div>
+
+
+<div class="content-wrap thirdwrap">
+   <div class="container">
+      
+      <div class="row">
+         <div class="col-md-7">
+            <div class="text-box">
+               <h2>We do the service</h2>
+                  <div class="text">
+                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Ammelier is a service for people with allergies and don't like to book their restaurants, we have a solution for this, we call it Ammelier.</p>
+                  </div>
+               <div class="button"><a href="#">check it out</a></div>
+            </div>
+         </div>
+         
+         <div class="col-md-5">
+            <div class="text-box">
+               <div class="box-img"></div>
+            </div>
+         </div>
+      </div>
+
+   </div>
+</div>
+
+
 
 <div class="fullscreen-content-wrap three">
    your content on screen 4
