7096365e60bda5b1da386ba2f06f552226c9bbc2
Lars Boesenach <lars@lokaalgevonden.nl>
2016-04-11 18:01:25 +0200

    almost finished

--- index.php
+++ index.php
@@ -11,7 +11,6 @@
 
 <div class="fullscreen-content-wrap one">
 	<div class="bg-img"></div>
-	<div class="darkscreen"></div>
    
    <div class="logo-wrap">
    		<div class="logo">
@@ -25,7 +24,7 @@
    		<div class="header-text">
    			<h1>Eat with confidence</h1>
    			<div class="p-text">
-	   			<p>Ammelier is a service for people with allergies and don't like to book their restaurants, we have a solution for this, we call it Ammelier.</p>
+	   			<p>Just as a Sommelier would assist you to choose the best wine for your meal, Ammelier assists you to choose the best meal to match your diet.</p>
 	   		</div>
    		</div>
    </div>
@@ -33,12 +32,27 @@
    <div class="Ancordown">
    		<a href="#ancor2">
             <div class="arrow-wrap">
-               <div class="arrow"></div>
+               <div class="arrow"><img src="img/arrow.svg" alt=""></div>
             </div>
          </a>
       		<p>find out how it works</p>
    </div>
 </div>
+
+
+<div class="content-wrap intro">
+   <div class="container">
+         
+         <div class="text-box">
+               <h2>We are building a service that suits your needs</h2>
+               <div class="text">
+                  <p>At last something special for people with food allergies, intolerances and dietary restrictions! Ammelier is your personal assistant in finding the best restaurants & hotels serving “free-from” meals. </p>
+               </div>
+         </div>
+
+   </div>
+</div>
+
 
 
 <div class="content-wrap" id="ancor2">
@@ -47,17 +61,22 @@
       <div class="row">
          <div class="col-md-7">
             <div class="text-box">
-               <h2>We understand nutrition</h2>
+               <h2>Restaurants and Hotels</h2>
                   <div class="text">
-                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Ammelier is a service for people with allergies and don't like to book their restaurants, we have a solution for this, we call it Ammelier.</p>
+                     <p>Ammelier helps you to find restaurants & hotels where chefs are happy to accommodate to your culinary needs. 
+Ammelier gives recommendations, all to save you time in tedious search for a safe place to eat for you, your family and friends. 
+If you love travelling and if you love food, Ammelier will assist you so that your diet will no longer be in a way. 
+Ammelier also utilizes innovative beacon technology to assist personalized customer’ experience.</p>
                   </div>
-               <div class="button"><a href="#">visit blog</a></div>
+               <div class="button"><a href="#">About Beacon</a></div>
             </div>
          </div>
          
          <div class="col-md-5">
             <div class="text-box">
-               <div class="box-img"></div>
+               <div class="box-img">
+                  <img src="img/img01@2x.png">
+               </div>
             </div>
          </div>
       </div>
@@ -72,17 +91,19 @@
    
    <div class="container">
       <div class="row">
-         <div class="col-md-5"></div>
-
          <div class="col-md-7">
             <div class="text-box">
-               <h2>We understand nutrition</h2>
+               <h2>Meet the Chef</h2>
                   <div class="text">
-                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Ammelier is a service for people with allergies and don't like to book their restaurants, we have a solution for this, we call it Ammelier.</p>
+                     <p>Ammelier connects you with people who understand! 
+Excellent chefs worldwide have taken up the challenge to prepare something amazing for you! It’s our pleasure to introduce you to the most caring chefs in London. Chefs we feature care about your individual diet, care about you, care about great taste and the best ingredients. </p>
+
+                     <p class="extra-text cursive-text">Con Gusto Catering is a London based luxury caterer specialising in European cuisine. Using only the freshest of seasonal ingredients, each menu is bespoke for the client, meaning we can tailor exactly to the clients’ needs. The chefs have over 15 years experience and love creating exciting new menus! Rigel Tufa, Head Chef, is a Michelin trained chef with experience throughout Italy, Canada, and the UK.</p>
                   </div>
-               <div class="button"><a href="#">check website</a></div>
+               <div class="button"><a href="#">About the chef</a></div>
             </div>
          </div>         
+
       </div>
    </div>
 </div>
@@ -94,17 +115,19 @@
       <div class="row">
          <div class="col-md-7">
             <div class="text-box">
-               <h2>We do the service</h2>
+               <h2>Experiences and Nutrition</h2>
                   <div class="text">
-                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Ammelier is a service for people with allergies and don't like to book their restaurants, we have a solution for this, we call it Ammelier.</p>
+                     <p>Ammelier also provides information about what’s in your meal.  Imagine you’re just having an amazing breakfast with your partner, and suddenly you’re coming up with reaction... It would ruin your day, to say the least.  Our nutritionist will provide recommendations for you to prevent it happening, amazing ingredients great to boost your health as well as delicious meals you can find around London. </p>
+
+                     <p class="extra-text">Ammelier will continuously seek out for you hand-picked unique experiences, all serving ‘free- from’ ingredients.  You’d be pleased to know that Ammelier has partnered with TellSpec so that you will know exactly what’s in your food. </p>
                   </div>
-               <div class="button"><a href="#">check it out</a></div>
+               <div class="button"><a href="#">About Tellspec</a></div>
             </div>
          </div>
          
          <div class="col-md-5">
             <div class="text-box">
-               <div class="box-img"></div>
+               <div class="box-img"><img src="img/img02@2x.png" alt=""></div>
             </div>
          </div>
       </div>
@@ -119,9 +142,9 @@
       <div class="newsletter-box">
          
          <div class="text-box">
-               <h2>Newsletter</h2>
+               <h2>Stay in Touche</h2>
                   <div class="text">
-                     <p>Intrested? We send a curated newsletter about London events every week.</p>
+                     <p>Intrested? stay informed about Ammelier. </p>
                   </div>
                <div class="button"><a href="#">subscribe</a></div>
 
@@ -138,14 +161,13 @@
 
 <div class="fullscreen-content-wrap footer">
    <div class="bg-img"></div>
-   <div class="darkscreen"></div>
    <div class="darkscreen-gradient"></div>
 
    <div class="footer-content">
          <div class="footer-text">
-            <h1>Have a dining experience you can only dream of.</h1>
+            <h1>We are building a service to suit your needs.</h1>
             <div class="p-text">
-               <p>Ammelier is a service for people with allergies and don't like to book their restaurants, we have a solution for this, we call it Ammelier.</p>
+               <p>Whether it’s gluten-free or diary-free diet or any other allergy or intolerance. Ammelier will be happy to help you.</p>
             </div>
 
             <div class="button-signup">
