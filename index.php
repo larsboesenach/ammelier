1285c1f730972ad7b03608993f09af664efe43a8
Lars Boesenach <lars@lokaalgevonden.nl>
2016-04-11 19:24:05 +0200

    asaas

--- index.php
+++ index.php
@@ -35,7 +35,7 @@
                <div class="arrow"><img src="img/arrow.svg" alt=""></div>
             </div>
          </a>
-      		<p>find out how it works</p>
+      	<p>find out how it works</p>
    </div>
 </div>
 
@@ -62,13 +62,17 @@
          <div class="col-md-7">
             <div class="text-box">
                <h2>Restaurants and Hotels</h2>
-                  <div class="text">
-                     <p>Ammelier helps you to find restaurants & hotels where chefs are happy to accommodate to your culinary needs. 
+               
+               <div class="text">
+                  <p>Ammelier helps you to find restaurants & hotels where chefs are happy to accommodate to your culinary needs. 
 Ammelier gives recommendations, all to save you time in tedious search for a safe place to eat for you, your family and friends. 
 If you love travelling and if you love food, Ammelier will assist you so that your diet will no longer be in a way. 
 Ammelier also utilizes innovative beacon technology to assist personalized customer’ experience.</p>
-                  </div>
-               <div class="button"><a href="#">About Beacon</a></div>
+               </div>
+               
+               <div class="button">
+                  <a href="http://estimote.com/" target="_blank">About Beacon</a>
+               </div>
             </div>
          </div>
          
@@ -94,13 +98,17 @@ Ammelier also utilizes innovative beacon technology to assist personalized custo
          <div class="col-md-7">
             <div class="text-box">
                <h2>Meet the Chef</h2>
-                  <div class="text">
-                     <p>Ammelier connects you with people who understand! 
+               
+               <div class="text">
+                  <p>Ammelier connects you with people who understand! 
 Excellent chefs worldwide have taken up the challenge to prepare something amazing for you! It’s our pleasure to introduce you to the most caring chefs in London. Chefs we feature care about your individual diet, care about you, care about great taste and the best ingredients. </p>
 
-                     <p class="extra-text cursive-text">Con Gusto Catering is a London based luxury caterer specialising in European cuisine. Using only the freshest of seasonal ingredients, each menu is bespoke for the client, meaning we can tailor exactly to the clients’ needs. The chefs have over 15 years experience and love creating exciting new menus! Rigel Tufa, Head Chef, is a Michelin trained chef with experience throughout Italy, Canada, and the UK.</p>
-                  </div>
-               <div class="button"><a href="#">About the chef</a></div>
+                  <p class="extra-text cursive-text">Con Gusto Catering is a London based luxury caterer specialising in European cuisine. Using only the freshest of seasonal ingredients, each menu is bespoke for the client, meaning we can tailor exactly to the clients’ needs. The chefs have over 15 years experience and love creating exciting new menus! Rigel Tufa, Head Chef, is a Michelin trained chef with experience throughout Italy, Canada, and the UK.</p>
+               </div>
+               
+               <div class="button">
+                  <a target="_blank" href="http://www.congustocatering.co.uk">About the chef</a>
+               </div>
             </div>
          </div>         
 
@@ -116,12 +124,16 @@ Excellent chefs worldwide have taken up the challenge to prepare something amazi
          <div class="col-md-7">
             <div class="text-box">
                <h2>Experiences and Nutrition</h2>
-                  <div class="text">
-                     <p>Ammelier also provides information about what’s in your meal.  Imagine you’re just having an amazing breakfast with your partner, and suddenly you’re coming up with reaction... It would ruin your day, to say the least.  Our nutritionist will provide recommendations for you to prevent it happening, amazing ingredients great to boost your health as well as delicious meals you can find around London. </p>
+               
+               <div class="text">
+                  <p>Ammelier also provides information about what’s in your meal.  Imagine you’re just having an amazing breakfast with your partner, and suddenly you’re coming up with reaction... It would ruin your day, to say the least.  Our nutritionist will provide recommendations for you to prevent it happening, amazing ingredients great to boost your health as well as delicious meals you can find around London. </p>
 
-                     <p class="extra-text">Ammelier will continuously seek out for you hand-picked unique experiences, all serving ‘free- from’ ingredients.  You’d be pleased to know that Ammelier has partnered with TellSpec so that you will know exactly what’s in your food. </p>
-                  </div>
-               <div class="button"><a href="#">About Tellspec</a></div>
+                  <p class="extra-text">Ammelier will continuously seek out for you hand-picked unique experiences, all serving ‘free- from’ ingredients.  You’d be pleased to know that Ammelier has partnered with TellSpec so that you will know exactly what’s in your food. </p>
+               </div>
+               
+               <div class="button">
+                  <a target="_blank" href="http://tellspec.com">About Tellspec</a>
+               </div>
             </div>
          </div>
          
@@ -143,13 +155,17 @@ Excellent chefs worldwide have taken up the challenge to prepare something amazi
          
          <div class="text-box">
                <h2>Stay in Touche</h2>
-                  <div class="text">
-                     <p>Intrested? stay informed about Ammelier. </p>
-                  </div>
-               <div class="button"><a href="#">subscribe</a></div>
+            
+               <div class="text">
+                  <p>Intrested? stay informed about Ammelier. </p>
+               </div>
+            
+               <div class="button">
+                  <a target="_blank" href="http://eepurl.com/bXxieD">subscribe</a>
+               </div>
 
                <div class="text cursive">
-                     <p>We will keep your information private.</p>
+                  <p>We will keep your information private.</p>
                </div>
             </div>
 
@@ -172,7 +188,7 @@ Excellent chefs worldwide have taken up the challenge to prepare something amazi
                </div>
 
                <div class="button-signup">
-                  <a id="open-popup">SIGNUP</a>
+                  <a href="http://eepurl.com/bXxieD" target="_blank" id="open-popup">SIGNUP</a>
                </div>
             </div>
       </div>
